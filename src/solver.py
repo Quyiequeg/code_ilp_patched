f07a2436122f5672a9f8f8df3a5462453c63392d
Quyiequeg <89134589+Quyiequeg@users.noreply.github.com>
2025-12-11 07:19:55 +0100

    save for pc umzug

--- src/solver.py
+++ src/solver.py
@@ -3,9 +3,6 @@ import numpy as np
 # from cvxopt import matrix, solvers
 import cvxpy as cp
 
-#open source solver: SCIP, HIGHS, CYLP
-# installieren mit: python -m pip install pulp[open_py]
-
 # Create two scalar optimization variables.
 x = cp.Variable()
 y = cp.Variable()
@@ -23,31 +20,3 @@ prob.solve()  # Returns the optimal value.
 print("status:", prob.status)
 print("optimal value", prob.value)
 print("optimal var", x.value, y.value)
-
-k = ...  # Dimension
-w = ...  # Gewichtsmatrix w_{i,j} als numpy Array (k x k)
-span_matrix = ...  # Matrix mit span(a_l, a_h) Werten (k x k)
-
-# Optimierungsvariable x der Form k x k
-x = cp.Variable((k, k))
-
-# Variable zur Akkumulation der Summe
-obj_expr = 0
-
-for i in range(k):
-    for j in range(k):
-        for l in range(k):
-            for h in range(k):
-                obj_expr += x[i, l] * x[j, h] * span_matrix[l, h] * w[i, j]
-
-objective = cp.Minimize(obj_expr)
-
-# Optional: Constraints definieren
-constraints = []
-
-problem = cp.Problem(objective, constraints)
-
-result = problem.solve()
-
-print("Minimaler Zielfunktionswert:", result)
-print("Optimale Lösung x:", x.value)
