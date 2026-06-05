c0631f8dbf0f71c868faff000cd1b9f6096c0d8f
Quyiequeg <89134589+Quyiequeg@users.noreply.github.com>
2026-06-05 19:32:34 +0200

    Revert "Reapply "expanded + achsenerweiterung implementiert""
    This reverts commit d5e22a834eca69bd1ba298d3b90230faf78384d3.

--- src/crossing_minimization.py
+++ src/crossing_minimization.py
@@ -271,8 +271,7 @@ def _sweep(layout: HivePlotLayout, neighborhood_map: dict[int | str, list[int | 
     # initialisierung layout parameter
     phi = layout.axis_order
     reversed_phi = list(reversed(phi))
-    # node_groups = layout.node_groups OHNE EXPANDED!
-    node_groups = layout.node_groups_expanded
+    node_groups = layout.node_groups
     dummy_node_groups = layout.node_groups_dummies
     state_set = set() # states sammeln um osszilation von zuständen zu erkennen
     # sweep-logik für reale knoten:
@@ -409,7 +408,7 @@ def intra_axis_handler(layout: HivePlotLayout) -> None:
             raise ValueError(f"Bei {path} handelt es sich um einen isolierten Knoten oder etwas Seltsames!!")
     
     layout.intra_axis_nodes = {key: sorted_intra_nodes_short[key] + sorted_intra_nodes_long[key] for key in intra_nodes} # vorarbeit zum finish der achsenordnung ->  triviale intra-axis| cluster intra-axis
-
+    ########################## //\\ ########################## FEHLT IM ILP!!!!!!!!!!!!!!!!!!!
 def calculate_barycenter_position(layout: HivePlotLayout, neighbor_group: list[int], node_axis_map) -> float:
     """Berechnet die Barycenterposition eines Knotens über seine ermittelten Nachbarn. Siehe HivePlotLayout.get_proper_neighbors().
 
@@ -454,15 +453,12 @@ def barycenter_crossmin_pipeline(layout: HivePlotLayout, threshold: float = floa
     # 2.
     node_position_map, node_axis_map = node_to_axis_maps(layout, layout.node_groups)
     neighborhood_map = layout.get_proper_neighborhood_map(layout.edges()) # initialisieren aus layout.graph
-    layout.expand_axes(node_axis_map)
-    node_position_map, node_axis_map = node_to_axis_maps(layout, layout.node_groups_expanded) # FÜR EXPAND
-    neighborhood_map = layout.get_proper_neighborhood_map(layout.edges(), expanded=True) # FÜR EXPAND
     subdivide_long_edges(layout, node_position_map, node_axis_map, neighborhood_map)
     # 3.
     fused_edge_list = layout.fuse_edges_with_edge_dummies() # dummykanten einbeziehen
     fused_node_list = layout.fuse_node_groups_with_dummies() # UPDATE !!!
     node_position_map, node_axis_map = node_to_axis_maps(layout, fused_node_list) # UPDATE !!!
-    neighborhood_map = layout.get_proper_neighborhood_map(fused_edge_list, expanded=True)
+    neighborhood_map = layout.get_proper_neighborhood_map(fused_edge_list)
     layout.dummy_edge_segments = layout.fuse_edges_with_edge_dummies()
     # 4.
     _sweep(layout, neighborhood_map, node_axis_map, threshold=threshold, real=True) # nur real
@@ -473,10 +469,12 @@ def barycenter_crossmin_pipeline(layout: HivePlotLayout, threshold: float = floa
     # 6.
     finish_structured_axis_orders(layout, isolated_nodes)
 
+def expand_axes():
+    pass
+
 if __name__ == "__main__":
-    from src.cost import cost_function_whole
     print("##########################################")
-    printer = 0 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< PRINTER
+    printer = 1 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< PRINTER
     # graph_mode = 0
     # graph_mode = 1
     graph_mode = 2
@@ -497,8 +495,6 @@ if __name__ == "__main__":
         node_groups=ng
     )
     print(hpl)
-    print("cost vorher")
-    # print(cost_function_whole(hpl.axis_order, hpl.fuse_node_groups_with_dummies(), hpl.fuse_edges_with_edge_dummies()))
     if printer == 1:
         render_debug(hpl, title="ORIGINAL") 
     hpl.axis_order = brute_force_ordering(axes, ng, list(G.edges()))
@@ -514,6 +510,4 @@ if __name__ == "__main__":
     print("Layout NACH OPTIMIERUNG")
     print(hpl)
     print(hpl.edges())
-    print("cost nachher")
-    # print(cost_function_whole(hpl.axis_order, hpl.fuse_node_groups_with_dummies(), hpl.fuse_edges_with_edge_dummies()))
     print("##########################################")
--- src/hiveplot.py
+++ src/hiveplot.py
@@ -37,9 +37,8 @@ class HivePlotLayout:
     node_order: dict[int, list[int]] = field(default_factory=dict)
 
     # unabhängige Kopien für pi+ und pi-, da diese sich im erweiterten Modell unterscheiden können
-    node_groups_expanded:  dict[int, list[int | str]] = field(default_factory=dict)
-    edges_expanded: list[tuple[int | str, int | str]] = field(default_factory=list)
-
+    node_order_plus:  dict[int, list[int]] = field(default_factory=dict) # +
+    node_order_minus: dict[int, list[int]] = field(default_factory=dict) # -
 
     # ergebnisse zu evaluationszwecken !prüfen
     crossings: Optional[int] = None
@@ -63,8 +62,8 @@ class HivePlotLayout:
             f"  Intra-Axis Edges: {self.intra_axis_edges}",
             f"  Long Edges: {self.long_edges}",
             f"  Node Order (pi_i): {self.node_order}",
-            f"  node_groups_expanded: {self.node_groups_expanded}",
-            f"  edges_expanded: {self.edges_expanded}",
+            f"  Node Order Plus (pi_i^+): {self.node_order_plus}",
+            f"  Node Order Minus (pi_i^-): {self.node_order_minus}",
             f"  Crossings (standard): {self.crossings}",
             f"  Crossings (erweitert): {self.crossings_extended}"
         ]
@@ -107,23 +106,17 @@ class HivePlotLayout:
         else:
             return self.node_groups.get(axis, [])
     
-    def fuse_node_groups_with_dummies(self, expanded: bool = True) -> dict[int, list[int]]:
+    def fuse_node_groups_with_dummies(self) -> dict[int, list[int]]:
         """Erzeugt ein dict mit Achsen als Schlüssel und der vereinigten Menge aus Knoten und Dummyknoten pro Achse als Wert. Zuerst die realen dann die virtuellen Knoten.
 
         Returns:
             dict[int, list[int]]: Vereinigung der Knoten und Dummyknoten pro Achse.
         """
         fused_groups = {}
-        if expanded:
-            for axis in self.axis_order:
-                original_nodes = self.node_groups_expanded.get(axis, [])
-                dummy_nodes = self.node_groups_dummies.get(axis, [])
-                fused_groups[axis] = list(original_nodes) + list(dummy_nodes)
-        else:
-            for axis in self.axis_order:
-                original_nodes = self.node_groups.get(axis, [])
-                dummy_nodes = self.node_groups_dummies.get(axis, [])
-                fused_groups[axis] = list(original_nodes) + list(dummy_nodes)
+        for axis in self.axis_order:
+            original_nodes = self.node_groups.get(axis, [])
+            dummy_nodes = self.node_groups_dummies.get(axis, [])
+            fused_groups[axis] = list(original_nodes) + list(dummy_nodes)
         return fused_groups
 
     def fuse_edges_with_edge_dummies(self) -> list[tuple[int, int]]:
@@ -136,7 +129,7 @@ class HivePlotLayout:
         fused_edges = direct_edges + self.dummy_edge_segments
         return fused_edges
     
-    def get_proper_neighborhood_map(self, fused_edge_list: list[tuple[int | str, int | str]], expanded: bool = False) -> dict[int | str: list[int | str]]:
+    def get_proper_neighborhood_map(self, fused_edge_list: list[tuple[int | str, int | str]]) -> dict[int | str: list[int | str]]:
         """Die Funktion ermittelt eine Liste, die jeden Knoten (sowohl real als auch virtuell) auf eine Liste seiner Nachbarn mappt.
 
         Args:
@@ -145,7 +138,7 @@ class HivePlotLayout:
             dict[int | str: list[int | str]]: KnotenID: Nachbarliste
         """
         neighbor_map = {}
-        fused_node_groups = self.fuse_node_groups_with_dummies(expanded)
+        fused_node_groups = self.fuse_node_groups_with_dummies()
         node_list = self.updated_nodes(fused_node_groups)
         for node in node_list:
             neighbor_map[node] = set()
@@ -170,107 +163,24 @@ class HivePlotLayout:
         #     neighbor_map[node] = neighbors_set
         # return neighbor_map
 
-    def expand_axes(self, node_axis_map) -> None:
-        edge_axis_map = {edge: (node_axis_map[edge[0]], node_axis_map[edge[1]]) for edge in self.edges()} # initialisiere kanten zu achsen map
-        expandable_axes_map = {}
-        inter_expandables = {}
-        node_groups_expanded = self.node_groups_expanded
-        node_groups = self.node_groups
-        # print(f"edge axis:{edge_axis_map}")
-        for edge in edge_axis_map: # key = knotenpaar, filter nach intra axis kanten
-            edge_positions = edge_axis_map[edge]
-            if edge_positions[0] == edge_positions[1]:
-                 expandable_axes_map.setdefault(edge_positions[0], []).append(edge) # check ob key vorhandenen + append
-        print(f"Expandable axes:{expandable_axes_map}")
-        for edge in edge_axis_map:
-            edge_positions = edge_axis_map[edge]
-            if edge_positions[0] == edge_positions[1]:
-                pass
-            elif edge_positions[0] in expandable_axes_map:
-                inter_expandables.setdefault(edge_positions[0], []).append(edge) # check ob key vorhandenen + append
-            elif edge_positions[1] in expandable_axes_map:
-                inter_expandables.setdefault(edge_positions[1], []).append(edge) # check ob key vorhandenen + append
-        print(f"Inter-expandables:{inter_expandables}")
-        for key in node_groups:
-            if key not in expandable_axes_map:
-                node_groups_expanded[key] = node_groups[key]
-            elif key in expandable_axes_map:
-                node_groups_expanded[(key, 0)] = node_groups[key] # pi^- links
-                node_groups_expanded[(key, 1)] = [-node for node in node_groups[key]] # pi^- rechts
-        print(f"node_groups_expanded:{node_groups_expanded}")
-        self.axis_order = list(node_groups_expanded.keys())
-        self.num_axes = len(self.axis_order)
-        axis_position_map = {}
-        for i, axis in enumerate(self.axis_order): # achsenid: position in phi
-            axis_position_map[axis] = i
-        print(f"axis_position_map:{axis_position_map}")
-
-        # die mit expandierten achsen verbundenen kanten aus dem hiveplotlayout entfernen und in korrektem format wieder hineinschreiben
-        for axis in expandable_axes_map:
-            self.graph.remove_edges_from(expandable_axes_map[axis])
-            new_intra_edges = []
-            for edge in expandable_axes_map[axis]: # expandierte achse und ihre intra knoten als
-                new_intra_edges.append((-edge[0], edge[1]))
-                new_intra_edges.append((edge[0], -edge[1]))
-            self.graph.add_edges_from(new_intra_edges)
-        
-        for axis in inter_expandables:
-            self.graph.remove_edges_from(inter_expandables[axis])
-            new_inter_edges = []
-            for edge in inter_expandables[axis]:
-                k = self.num_axes
-                axis_u = edge_axis_map[edge][0]
-                axis_v = edge_axis_map[edge][1]
-                pos_axis_u = axis_position_map.get(axis_u, axis_position_map.get((axis_u, 0))) # beide fälle müssen abgedeckt sein: achse ist int und achse ist expandiert und tupel
-                pos_axis_v = axis_position_map.get(axis_v, axis_position_map.get((axis_v, 0)))
-                span_uv = (pos_axis_u - pos_axis_v) % k
-                span_vu = (pos_axis_v - pos_axis_u) % k
-                if axis_u in expandable_axes_map and axis_v in expandable_axes_map: # ziel- und startachse expandiert
-                    if span_uv <= span_vu: # v -> u + bei gleichstand immer links
-                        new_inter_edges.append((edge[0], -edge[1]))
-                    elif span_vu < span_uv: # u auf expandierter achse und u -> v
-                        new_inter_edges.append((-edge[0], edge[1]))
-                else: # ziel- oder startkante expandiert
-                    if axis_u == axis:
-                        if span_uv <= span_vu: # u auf expandierter achse und v -> u + bei gleichstand immer links
-                            new_inter_edges.append((edge[0], edge[1]))
-                        elif span_vu < span_uv: # u auf expandierter achse und u -> v
-                            new_inter_edges.append((-edge[0], edge[1]))
-                    elif axis_v == axis:
-                        if span_vu <= span_uv: # v auf expandierter achse und u -> v
-                            new_inter_edges.append((edge[0], edge[1]))
-                        elif span_vu < span_uv: # v auf expandierter achse und v -> u
-                            new_inter_edges.append((edge[0], -edge[1]))
-            self.graph.add_edges_from(new_inter_edges)
-        # die mit expandierten achsen verbundenen kanten in neues format bringen und wieder in das hiveplotlayout schreiben
-        
-
-
-
-
-    
 if __name__ == "__main__":
-    from src.graphs import sample_graph_multipartite, sample_graph_selfconstructed_extended
-    from src.ordering import native_order, node_groups, node_to_axis_maps, brute_force_ordering, reordered_node_groups
-    graph_mode = 2
-    G = sample_graph_selfconstructed_extended(graph_mode)
+    from src.graphs import sample_graph_multipartite
+    from src.ordering import native_order, node_groups
+
+    G = sample_graph_multipartite()
     nodes = list(G.nodes(data="subset"))
-    axes = native_order(nodes)
-    ng = node_groups(nodes)
-    # print("Layout ORIGINAL")
+    phi = native_order(nodes)
+    grps = node_groups(nodes)
+
     hpl = HivePlotLayout(
         graph=G,
-        num_axes=len(axes),
-        axis_order=axes,
-        node_groups=ng
+        num_axes=len(phi),
+        axis_order=phi,
+        node_groups=grps
     )
-    hpl.axis_order = brute_force_ordering(axes, ng, list(G.edges()))
-    hpl.node_groups = reordered_node_groups(ng, hpl.axis_order)
-    # isolated_nodes = cm.remove_isolated_nodes(layout.graph, layout.node_groups)
-    node_position_map, node_axis_map = node_to_axis_maps(hpl, hpl.node_groups)
-    hpl.expand_axes(node_axis_map)
-    # print(hpl)
-    print(hpl.edges())
     print("##########################################")
+    print(hpl)
     print("##########################################")
-    # print("##########################################")
+    print("Achsen:", hpl.axis_order)
+    print("Knoten auf Achse 0:", hpl.nodes_on_axis(0))
+    print("##########################################")
